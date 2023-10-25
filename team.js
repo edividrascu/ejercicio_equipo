a2122f6af3e90c8ace358e377673e0fddbd88862
Manuel <manu.benitez1@hotmail.com>
2023-10-25 12:41:05 +0200

    caso de prueba

--- team.js
+++ team.js
@@ -22,5 +22,17 @@ const teamMembers = [
     favoriteFilm: "Spirited Away",
     favoriteBook: "Cien años de Soledad",
     petName: "Haru",
+  },
+  {
+    name: "Michelle",
+    surname: "Caicedo",
+    age: "30",
+    city: "Valencia",
+    hobby: "Pintar",
+    favoriteFood: "Hamburguesas",
+    favoriteVideoGame: "Sims",
+    favoriteFilm: "Spirited Away",
+    favoriteBook: "Cien años de Soledad",
+    petName: "Haru",
   }
 ];
